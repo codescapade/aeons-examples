0de810050eebcd9bd95af073e135f4f8a264ddf0
Jurien Meerlo <codescapade@gmail.com>
2022-03-21 01:12:40 +0000

    tile layers are rendering.

--- examples/ldtk-import/source/scenes/GameScene.hx
+++ examples/ldtk-import/source/scenes/GameScene.hx
@@ -1,13 +1,9 @@
 package scenes;
 
-import aeons.math.FastMatrix4;
-import ldtk.Level;
-import aeons.graphics.Color;
-import aeons.graphics.RenderTarget;
-import ldtk.Layer_Tiles;
+import aeons.components.CLdtkTilemap;
+import aeons.tilemap.ldtk.LdtkLayer;
 import aeons.tilemap.Tileset;
 import aeons.graphics.atlas.Atlas;
-import components.CTest;
 import aeons.Aeons;
 import aeons.components.CCamera;
 import aeons.components.CTransform;
@@ -19,64 +15,34 @@ class GameScene extends Scene {
 
   var tileset: Tileset;
 
-  var layer: Layer_Tiles;
+  var tileLayer: LdtkLayer;
 
-  var xOffset: Int;
-  var yOffset: Int;
+  var autoLayer: LdtkLayer;
+
+  var fullAutoLayer: LdtkLayer;
 
   public override function init() {
     addSystem(new RenderSystem());
 
     createCamera();
     
-    var test = new Ldtk();
-    var level = test.all_levels.First_Level;
+    var world = new Ldtk();
+    var level = world.all_levels.First_Level;
 
     tileset = Tileset.fromLdtkTileset(level.l_Test.tileset);
-    layer = level.l_Test;
-    xOffset = level.worldX;
-    yOffset = level.worldY;
+    tileLayer = LdtkLayer.fromTilesLayer(level.l_Test, tileset);
+    autoLayer = LdtkLayer.fromIntAutoLayer(level.l_AutoTiles, tileset);
+    fullAutoLayer = LdtkLayer.fromAutoLayer(level.l_FullAuto, tileset);
 
-    Aeons.assets.loadAtlas('atlas', (atlas: Atlas) -> {
-      var e = addEntity(new Entity());
-      e.addComponent(new CTransform({ x: 100, y: 100 }));
-      e.addComponent(new CTest({ atlas: atlas, frameName: 'bunny' }));
-    });
-  }
-
-  public override function render(target:RenderTarget) {
-    super.render(target);
-
-    target.transform.setFrom(FastMatrix4.identity());
-    target.start();
-    renderLayerTiles(target, xOffset, yOffset, layer, tileset);
-    target.present();
+    var entity = addEntity(new Entity());
+    entity.addComponent(new CTransform());
+    var tilemap = entity.addComponent(new CLdtkTilemap());
+    tilemap.addLayers([tileLayer, autoLayer, fullAutoLayer]);
   }
 
   function createCamera() {
     final eCamera = addEntity(new Entity());
     eCamera.addComponent(new CTransform());
     eCamera.addComponent(new CCamera());
-  }
-
-  function renderLayerTiles(target: RenderTarget, xOffset: Int, yOffset: Int, layer: Layer_Tiles, tileset: Tileset) {
-    for (cy in 0...layer.cHei) {
-      for (cx in 0...layer.cWid) {
-        if (layer.hasAnyTileAt(cx, cy)) {
-          for (tile in layer.getTileStackAt(cx, cy)) {
-            var flipX = tile.flipBits & 1 != 0 ? -1 : 1;
-            var flipOffsetX = flipX == 1 ? 0 : tileset.tileWidth;
-            var flipY = tile.flipBits & 2 != 0 ? -1 : 1;
-            var flipOffsetY = flipY == 1 ? 0 : tileset.tileHeight;
-            var tileFrame = tileset.getRect(tile.tileId);
-            
-            target.drawImageSectionWithSize(xOffset + cx * tileset.tileWidth + flipOffsetX,
-                yOffset + cy * tileset.tileHeight + flipOffsetY, tileset.tileWidth * flipX,
-                tileset.tileHeight * flipY, tileFrame.x, tileFrame.y, tileFrame.width, tileFrame.height,
-                tileset.tileImage, Color.White);
-          }
-        }
-      }
-    }
   }
 }
