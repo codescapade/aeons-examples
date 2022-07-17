eb46407d72ecefe05c46d13ad5e74ed3c9798d5d
Jurien Meerlo <codescapade@gmail.com>
2022-07-18 00:21:39 +0100

    updated examples to use the aeons.toml config file

--- examples/demos/bunnies/khafile.js
+++ examples/demos/bunnies/khafile.js
@@ -1,28 +1,26 @@
-// A new Kha project.
+
 let project = new Project('bunnies');
 
-// Make it possible to have the same file name in different folders.
 project.addAssets('assets/**', {
-    nameBaseDir: 'assets',
-    destination: '{dir}/{name}',
-    name: '{dir}/{name}'
+  nameBaseDir: 'assets',
+  destination: '{dir}/{name}',
+  name: '{dir}/{name}'
 });
 
-// Shader folder.
+project.icon = 'icon.png';
+
 project.addShaders('shaders/**');
 
-// Source folder.
 project.addSources('source');
 
-// Add the Aeons engine.
 project.addLibrary('aeons');
+
 
 project.addParameter('-dce full');
 
 project.targetOptions.html5.disableContextMenu = true;
-
-// This sets the canvas id and javascript name in the html output file.
 project.targetOptions.html5.canvasId = 'aeonsGame';
 project.targetOptions.html5.scriptName = 'aeons';
+
 
 resolve(project);
